1536941487d7dc9fee9de841e340df82f544560e
Shahriar <shhrrtnvr@gmail.com>
2021-04-18 06:24:52 +0600

    ok

---
+++ Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.board = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.newgame = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.player2score = new System.Windows.Forms.Button();
+            this.player2name = new System.Windows.Forms.Button();
+            this.player1score = new System.Windows.Forms.Button();
+            this.player1name = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.board.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.board.AutoSize = true;
+            this.board.BackColor = System.Drawing.Color.Lavender;
+            this.board.ColumnCount = 3;
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.board.Controls.Add(this.button9, 2, 2);
+            this.board.Controls.Add(this.button8, 1, 2);
+            this.board.Controls.Add(this.button7, 0, 2);
+            this.board.Controls.Add(this.button6, 2, 1);
+            this.board.Controls.Add(this.button5, 1, 1);
+            this.board.Controls.Add(this.button4, 0, 1);
+            this.board.Controls.Add(this.button3, 2, 0);
+            this.board.Controls.Add(this.button2, 1, 0);
+            this.board.Controls.Add(this.button1, 0, 0);
+            this.board.Location = new System.Drawing.Point(46, 53);
+            this.board.MinimumSize = new System.Drawing.Size(230, 230);
+            this.board.Name = "board";
+            this.board.RowCount = 3;
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.board.Size = new System.Drawing.Size(230, 230);
+            this.board.TabIndex = 0;
+            this.board.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Ivory;
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button9.Location = new System.Drawing.Point(155, 155);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(72, 72);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Ivory;
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(79, 155);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 72);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Ivory;
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(3, 155);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 72);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Ivory;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(155, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 70);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Ivory;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(79, 79);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 70);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Ivory;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(3, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Ivory;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(155, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 70);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Ivory;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(79, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Ivory;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.newgame);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Location = new System.Drawing.Point(49, 315);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(224, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // newgame
+            // 
+            this.newgame.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.newgame.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.newgame.Location = new System.Drawing.Point(118, 0);
+            this.newgame.Name = "newgame";
+            this.newgame.Size = new System.Drawing.Size(109, 39);
+            this.newgame.TabIndex = 1;
+            this.newgame.Text = "New Game";
+            this.newgame.UseVisualStyleBackColor = true;
+            this.newgame.Click += new System.EventHandler(this.newgame_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Exit.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.Exit.Location = new System.Drawing.Point(0, 0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(112, 40);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+            this.tableLayoutPanel1.Controls.Add(this.player2score, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.player2name, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.player1score, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.player1name, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(315, 140);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(157, 62);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // player2score
+            // 
+            this.player2score.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.player2score.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player2score.Location = new System.Drawing.Point(101, 34);
+            this.player2score.Name = "player2score";
+            this.player2score.Size = new System.Drawing.Size(53, 25);
+            this.player2score.TabIndex = 3;
+            this.player2score.Text = "0";
+            this.player2score.UseVisualStyleBackColor = true;
+            // 
+            // player2name
+            // 
+            this.player2name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.player2name.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.player2name.ForeColor = System.Drawing.Color.Crimson;
+            this.player2name.Location = new System.Drawing.Point(3, 34);
+            this.player2name.Name = "player2name";
+            this.player2name.Size = new System.Drawing.Size(92, 25);
+            this.player2name.TabIndex = 2;
+            this.player2name.Text = "Player 2";
+            this.player2name.UseVisualStyleBackColor = true;
+            // 
+            // player1score
+            // 
+            this.player1score.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.player1score.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player1score.Location = new System.Drawing.Point(101, 3);
+            this.player1score.Name = "player1score";
+            this.player1score.Size = new System.Drawing.Size(53, 25);
+            this.player1score.TabIndex = 1;
+            this.player1score.Text = "0";
+            this.player1score.UseVisualStyleBackColor = true;
+            // 
+            // player1name
+            // 
+            this.player1name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.player1name.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.player1name.ForeColor = System.Drawing.Color.Blue;
+            this.player1name.Location = new System.Drawing.Point(3, 3);
+            this.player1name.Name = "player1name";
+            this.player1name.Size = new System.Drawing.Size(92, 25);
+            this.player1name.TabIndex = 0;
+            this.player1name.Text = "Player 1";
+            this.player1name.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Tan;
+            this.label1.Location = new System.Drawing.Point(334, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score Board";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToe.Properties.Resources.bokeh_553439_960_720;
+            this.ClientSize = new System.Drawing.Size(484, 391);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.board);
+            this.MinimumSize = new System.Drawing.Size(500, 430);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe By Ahmed Shahriar Tanvir";
+            this.board.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel board;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button newgame;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button player2score;
+        private System.Windows.Forms.Button player2name;
+        private System.Windows.Forms.Button player1score;
+        private System.Windows.Forms.Button player1name;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string Symbol = "OX";
+        private static readonly Color[] Color = new Color[2] { System.Drawing.Color.Blue, System.Drawing.Color.Crimson };
+        private static readonly int[] Score = new[] { 0, 0 };
+
+        private int _player = 0, _moves = 0;
+        private int[,] _grid;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void newgame_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+        private void Init()
+        {
+            _player = 0;
+            _moves = 0;
+            _grid = new int[3, 3]
+            {
+                {10, 11, 12},
+                {13, 14, 15},
+                {16, 17, 18}
+            };
+            for (var i = 1; i < 10; i++)
+            {
+                var b = (Button)this.board.Controls["button" + i.ToString()];
+                b.Text = "";
+                b.Enabled = true;
+            }
+            ScoreRefresh();
+        }
+
+        private void ScoreRefresh()
+        {
+            this.player1score.Text = Score[0].ToString();
+            this.player2score.Text = Score[1].ToString();
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var clicked = (Button)sender;
+            if (clicked.Text != "") return;
+            clicked.Text = Symbol[_player].ToString();
+            clicked.ForeColor = Color[_player];
+            var btnIdx = Convert.ToInt32(clicked.Name[6].ToString()) - 1;
+
+            _grid[btnIdx / 3, btnIdx % 3] = _player + 1;
+            _moves++;
+
+            Checker();
+            _player ^= 1;
+            ScoreRefresh();
+        }
+        private bool Checker()
+        {
+            var winner = 0;
+            for (var i = 0; i < 3 && winner == 0; i++)
+            {
+                if (_grid[i, 0] == _grid[i, 1] && _grid[i, 1] == _grid[i, 2])
+                    winner = _grid[i, 0];
+                if (_grid[0, i] == _grid[1, i] && _grid[1, i] == _grid[2, i])
+                    winner = _grid[0, i];
+            }
+
+            if ((_grid[0, 0] == _grid[1, 1] && _grid[1, 1] == _grid[2, 2]) ||
+                (_grid[0, 2] == _grid[1, 1] && _grid[1, 1] == _grid[2, 0]))
+                winner = _grid[1, 1];
+
+            if (winner != 0)
+            {
+                DisableButtons();
+                Score[winner - 1]++;
+                MessageBox.Show("Winner is Player" + winner);
+            }
+            else if (_moves == 9)
+            {
+                DisableButtons();
+                MessageBox.Show("Game Drawn");
+            }
+
+            return winner != 0 || _moves == 9;
+        }
+
+        private void DisableButtons()
+        {
+            for (var i = 1; i < 10; i++)
+            {
+                var b = (Button)this.board.Controls["button" + i.ToString()];
+                var btnIdx = Convert.ToInt32(b.Name[6].ToString()) - 1;
+                if (_grid[btnIdx / 3, btnIdx % 3] > 3) b.Enabled = false;
+            }
+        }
+
+    }
+}
